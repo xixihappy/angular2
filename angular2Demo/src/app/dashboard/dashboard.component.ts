eda6ed3398394889290e7f358dc0bd7827eedea0
zhifang.hu <zhifang.hu@integrity.com.cn>
2017-07-19 15:38:53 +0800

    登录后的头部dom内容更改

--- angular2Demo/src/app/dashboard/dashboard.component.ts
+++ angular2Demo/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component,ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { UserService } from '../service/user.service';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  userName: string;
+  @ViewChild ('userName')
+  userdiv: ElementRef;
+  constructor(
+     private userService: UserService
+  ) { }
+  ngAfterViewInit() {
+    this.userService.getAuths()
+      .subscribe(data => {
+        console.info('数据是：', data);
+        this.userdiv.nativeElement.innerHTML = data.body.info.userName;
+      }, error => {
 
-  constructor() { }
-
+      });
+  }
   ngOnInit() {
+    
   }
 
 }
